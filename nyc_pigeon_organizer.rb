4e38f3b3e3c239f6f8333815db1d7c69e16aca9d
Yong Nam Kang <j.yongnamkang@gmail.com>
2019-01-26 21:42:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,9 +2,6 @@ require "pry"
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
   data.each do |category, info|
-    data[:gender].each do |sex, name|
-      name.each {|pigeon| pigeon_list[pigeon] = {color: [], gender: [sex.to_s], lives:[]}}
-    end
     info.each do |trait, name|
       name.each do |pigeon|
         pigeon_list[pigeon][category.to_sym] << trait.to_s
